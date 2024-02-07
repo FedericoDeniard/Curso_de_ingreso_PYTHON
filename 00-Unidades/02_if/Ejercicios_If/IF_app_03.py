030c50e4bcb83a4df80eb68bab0767b607a21efb
Federico Deniard <fededeniard@gmail.com>
2024-02-06 23:11:34 -0300

    IF 03

--- 00-Unidades/02_if/Ejercicios_If/IF_app_03.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_03.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre:     Federico
+apellido:   Deniard
 ---
 Ejercicio: if_03
 ---
@@ -34,7 +34,11 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        edad = int(self.txt_edad.get())
+        if edad >= 18:
+            alert("","MAYOR")
+        else:
+            alert("","MENOR")
 
         
         
