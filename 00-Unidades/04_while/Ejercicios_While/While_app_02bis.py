bf2d39e2110fc515810d84c22653e90e24c85ae0
Federico Deniard <fededeniard@gmail.com>
2024-02-15 11:14:29 -0300

    While 02bis

--- 00-Unidades/04_while/Ejercicios_While/While_app_02bis.py
+++ 00-Unidades/04_while/Ejercicios_While/While_app_02bis.py
@@ -6,8 +6,8 @@ import customtkinter
 
 
 '''
-nombre:
-apellido:
+nombre:     Federico
+apellido:   Deniard
 ---
 Ejercicio: while_02bis
 ---
@@ -29,7 +29,10 @@ class App(customtkinter.CTk):
         
     
     def btn_mostrar_iteracion_on_click(self):
-        pass
+        contador = 0
+        while contador < 10:
+            contador +=2
+            alert("",contador)
     
     
 if __name__ == "__main__":
