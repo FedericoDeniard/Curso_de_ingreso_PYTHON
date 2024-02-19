fb488718bb2343e6158bf8fa7c3f70ad11f3a463
Federico Deniard <fededeniard@gmail.com>
2024-02-18 23:44:37 -0300

    While TP 2

--- 00-Unidades/04_while/TP_While/06_While_Elecciones_Paso.py
+++ 00-Unidades/04_while/TP_While/06_While_Elecciones_Paso.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre:     Federico
+apellido:   Deniard
 ---
 TP: While_elecciones_paso
 ---
@@ -35,7 +35,46 @@ class App(customtkinter.CTk):
         self.btn_validar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
 
     def btn_validar_on_click(self):
-        pass
+        mayor_votos = 0
+        candidato_mayor_votos = ""
+        menor_votos = None
+        candidato_menor_votos = ""
+        edad_menor_votos = 0
+        promedio_edad = 0
+        total_votos = 0
+        contador = 0
+        while True:
+            candidato = prompt("","Ingrese el nombre del candidato")
+            edad = prompt("","Ingrese la edad del candidato")
+            votos = prompt("","Ingrese la cantidad de votos")
+            if candidato == None or edad == None or votos == None:
+                break
+            edad = int(edad)
+            votos = int(votos)
+            promedio_edad += edad
+            contador += 1
+            total_votos += votos
+            if mayor_votos == None or menor_votos == None:
+                mayor_votos = votos
+                menor_votos = votos
+            elif votos > mayor_votos:
+                mayor_votos = votos
+                candidato_mayor_votos = candidato
+            elif votos < menor_votos:
+                menor_votos = votos
+                candidato_menor_votos = candidato
+                edad_menor_votos = edad
+            print("",f"menor votos: {menor_votos}, mayor votos:{mayor_votos}")
+
+        promedio_edad = promedio_edad / contador
+        alert("",f"El candidato con más votos es {candidato_mayor_votos} \n El candidato con menos votos es {candidato_menor_votos} y su edad es: {edad_menor_votos}\n El promedio de la edad de los candidatos es: {promedio_edad}\n El total de los votos fue de: {total_votos} ")
+        
+
+                
+
+
+
+
 
 
 if __name__ == "__main__":
